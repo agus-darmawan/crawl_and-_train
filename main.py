8a5524313a6c885f4358dc4a3b0698acbce4f306
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-11-26 07:08:54 +0700

    feat: add new data

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from module import google_map_review_scrapper
 
-url = 'https://www.google.com/maps/place/National+Monument/@-6.1753924,106.8249641,17z/data=!4m7!3m6!1s0x2e69f5d2e764b12d:0x3d2ad6e1e0e9bcc8!8m2!3d-6.1753924!4d106.8271528!9m1!1b1'
-scroll_limit = 10
+url = 'https://www.google.com/maps/place/City+of+Tomorrow+Mall/@-7.3453319,112.7249186,918m/data=!3m1!1e3!4m8!3m7!1s0x2dd7fca9c2171c9f:0xefadbc76ce524bdc!8m2!3d-7.3453319!4d112.7274935!9m1!1b1!16s%2Fm%2F05zww62?entry=ttu&g_ep=EgoyMDI0MTExOS4yIKXMDSoASAFQAw%3D%3D'
+
+scroll_limit = 20
 
 scrapper = google_map_review_scrapper
 
